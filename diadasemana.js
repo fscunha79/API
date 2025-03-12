58851075993ec60df2500d40a99ba42bb8ac8e06
fscunha79 <133812771+fscunha79@users.noreply.github.com>
2025-03-12 16:21:43 -0300

    Create diadasemana.js

---
+++ diadasemana.js
@@ -0,0 +1,26 @@
+// api/DiaDaSemana.js
+module.exports = (request, response) => {
+    const data = request.query.data;
+    if (!data) {
+        return response.send('Por favor, forneça uma data no formato dd/mm/aaaa.');
+    }
+
+    const partes = data.split('/');
+    if (partes.length !== 3) {
+        return response.send('Data inválida. Use o formato dd/mm/aaaa.');
+    }A
+
+    const dia = parseInt(partes[0], 10);
+    const mes = parseInt(partes[1], 10) - 1; // Meses em JavaScript são 0-indexados
+    const ano = parseInt(partes[2], 10);
+
+    const dataObj = new Date(ano, mes, dia);
+    const diasDaSemana = ['Domingo', 'Segunda-Feira', 'Terça-Feira', 'Quarta-Feira', 'Quinta-Feira', 'Sexta-Feira', 'Sábado'];
+
+    // return res.send(`O dia da semana para a data ${data} é: ${diasDaSemana[dataObj.getDay()]}`);
+
+    response.json({
+        data: data,
+        diaDaSemana: diasDaSemana[dataObj.getDay()]
+    });
+};
